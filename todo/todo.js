2260f93cfa26d6aa9f5de0d854fd92d8a5802d54
ChenYongYang <1214137301@qq.com>
2018-08-01 01:17:05 +0800

    add

--- todo/todo.js
+++ todo/todo.js
@@ -1,30 +1,36 @@
 const vm = new Vue({
     el: '#app',
     data: {
-        isDoubleClick: false,
         todo: '',
         cur: '',
         todos: [{
                 isCompleted: false,
-                content: '这是第一条',
+                content: '这是第1条',
             },
             {
                 isCompleted: false,
-                content: '这是第一条',
+                content: '这是第2条',
             },
             {
                 isCompleted: false,
-                content: '这是第一条',
+                content: '这是第3条',
             },
             {
                 isCompleted: false,
-                content: '这是第一条',
+                content: '这是第4条',
             },
-        ]
+        ],
+        hash: '',
     },
     created() { //应用于ajax获取 初始化数据
         //如果localStorage有数据就用有的，没有就用默认的
         this.todos = JSON.parse(localStorage.getItem('data')) || this.todos;
+        //监听hash值的变化
+        this.hash = window.location.hash.slice(2) || '';
+        window.addEventListener('hashchange', () => {
+            //当hash变化，重新操作记录的数据
+            this.hash = window.location.hash.slice(2);
+        }, false);
     },
     watch: {
         //将数据存储到本地，一旦数据有变化，就更新本地存储
@@ -40,6 +46,19 @@ const vm = new Vue({
     computed: {
         count() {
             return this.todos.filter(item => !item.isCompleted).length;
+        },
+        filterTodos() {
+            if (this.hash === 'all') {
+                return this.todos;
+            }
+            if (this.hash === 'finish') {
+                return this.todos.filter(item => item.isCompleted);
+            }
+            if (this.hash === 'unfinish') {
+                return this.todos.filter(item => !item.isCompleted);
+            }
+            //如果锚点是乱输入的，默认输出所有todo
+            return this.todos;
         }
     },
     methods: {
